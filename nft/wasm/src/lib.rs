9f2d0c90f3c9c6ba4e3dad3ed4e6da6190682da7
zahid ali <zahidali3170@gmail.com>
2022-08-30 18:57:17 +0500

    first

---
+++ nft/wasm/src/lib.rs
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    nft
+    (
+        callBack
+        buyNft
+        claimRoyaltiesFromMarketplace
+        createNft
+        getNftPrice
+        issueToken
+        setLocalRoles
+    )
+}
